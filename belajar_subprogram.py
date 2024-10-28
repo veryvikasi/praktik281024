409a11d834ed939a90fe31e9924e18cdb30b013b
ferihandani <sample@example.com>
2024-10-28 13:53:43 +0700

    git commit file pada stage fileferihandani

---
+++ belajar_subprogram.py
@@ -0,0 +1,6 @@
+def mencari_luas_lingkaran(jari-jari)
+    luas_lingkaran = 3.14 "jari-jari*jari-jari"
+    return  luas_lingkaran
+
+lingkaran_pertama=mencari_luas_lingkaran(5)
+print(lingkaran_pertama)
--- run_shell.sh
+++ run_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "---baris ini adalah shell scriptnya fery yang  di jalankan---"
-python3 hallo.py
+python3 hallo.py belajar_subprogram.py
 
